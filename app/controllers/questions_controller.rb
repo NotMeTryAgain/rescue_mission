9d5411c593b77d2b4f0b86231e78d5f964a56b64
Michael Hebert <michaelhebert@Michaels-MacBook-Pro.local>
2016-03-23 18:03:34 -0400

    edit function added for questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,7 @@
 class QuestionsController < ApplicationController
   def index
     @questions = Question.all.order('created_at DESC')
-    @answer = Answer.all.order('created_at DESC')
+    # @answer = Answer.all.order('created_at DESC')
   end
 
   def show
@@ -10,6 +10,10 @@ class QuestionsController < ApplicationController
 
   def new
     @question = Question.new
+  end
+
+  def edit
+    @question = Question.find(params[:id])
   end
 
   def create
@@ -21,6 +25,18 @@ class QuestionsController < ApplicationController
     else
       flash[:notice] = "#{@question.errors.full_messages.first}"
       render 'new'
+    end
+  end
+
+  def update
+    @question = Question.find(params[:id])
+
+    if @question.update(question_params)
+      flash[:notice] = "You have successfully updated your question!"
+      redirect_to @question
+    else
+      flash[:notice] = "#{@question.errors.full_messages.first}"
+      render 'edit'
     end
   end
 
